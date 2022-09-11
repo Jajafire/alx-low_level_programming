81b6fec241d934b4125f7f40998ef46280e21614
Sammy <sammywoyinka10@gmail.com>
2022-09-11 16:00:19 -0700

    o

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,13 +6,13 @@
  *  Return: 1
  */
 int main(void)
-		{
-		int s = strlen("and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
+{
+int s = strlen("and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
 
-		write(2,
-				"and that piece of art is useful\" - Dora Korper, 2015-10-19\n",
-				s);
-		return (1);
-		}
+write(2,
+		"and that piece of art is useful\" - Dora Korper, 2015-10-19\n",
+		s);
+return (1);
+}
 
 
