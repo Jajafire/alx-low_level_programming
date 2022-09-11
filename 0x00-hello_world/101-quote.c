6aa4cbb8b6da2f4b4d058c3c69930988b4367e18
Sammy <sammywoyinka10@gmail.com>
2022-09-11 15:46:05 -0700

    p

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-
+#include <string.h>
+#include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ *  main - main block
+ *  Return: 1
  */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+int main(void
+		{
+		int s = strlen("and that piece of art is useful\" - Dora Korper, 2015-10-19\n");
 
-	return (1);
-}
+		write(2,
+				"and that piece of art is useful\" - Dora Korper, 2015-10-19\n",
+				s);
+		}
+
+
