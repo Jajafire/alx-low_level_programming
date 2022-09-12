905e14176a95082e4660b5a77cb45b9a1e885586
Sammy <sammywoyinka10@gmail.com>
2022-09-11 19:16:41 -0700

    p

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,19 @@
  * You can only use the putchar
  * Return: 0
  */
+
 int main(void)
 
 {
 	int l = 'a';
+
 	while (l <= 'z')
 	{
 		putchar(l);
+
 		l += 1;
 	}
 	putchar('\n');
+
 	return (0);
 }
