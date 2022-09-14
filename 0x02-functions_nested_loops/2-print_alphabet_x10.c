80db8caf06c14327fdfc785c8e1f638f34711d7c
Sammy <sammywoyinka10@gmail.com>
2022-09-14 06:57:44 -0700

    l

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - alphabet
+ * print_alphabet_x10 - print a to z ten times
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
