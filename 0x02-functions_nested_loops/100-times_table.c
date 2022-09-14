246e4aa14c79c45b2ea71744c692c8e70a2ffaab
Sammy <sammywoyinka10@gmail.com>
2022-09-14 14:50:33 -0700

    k

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,9 +10,8 @@ void print_times_table(int n)
 
 {
 	int a = 0, rep, b;
-	
-	if (n < 0 || n > 15)
 
+	if (n < 0 || n > 15)
 		return;
 
 	while (a <= n)
@@ -20,7 +19,6 @@ void print_times_table(int n)
 		for (b = 0; b <= n; b++)
 		{
 			rep = a * b;
-
 			if  (b == 0)
 				_putchar('0' + rep);
 			else if (rep < 10)
@@ -30,7 +28,7 @@ void print_times_table(int n)
 				_putchar('0' + rep);
 			}
 			else if (rep < 100)
-			{	
+			{
 				_putchar(' ');
 				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
@@ -38,7 +36,7 @@ void print_times_table(int n)
 			else
 			{
 				_putchar('0' + rep / 100);
-				_putchar('0' + (rep - 100) / 10);										
+				_putchar('0' + (rep - 100) / 10);
 				_putchar('0' + rep % 10);
 			}
 			if (b < n)
