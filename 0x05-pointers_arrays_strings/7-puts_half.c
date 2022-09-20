8267c60ef714ba2dd61dd3238d3deee463fe76d8
Sammy <sammyawoyinka10@gmail.com>
2022-09-19 23:57:00 -0700

    j

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the half of a string
+ * followed by a newline
+ */
+
+void puts_half(char *str)
+{
+	unsigned long int start, len, i;
+
+	len = strlen(str);
+	start = len / 2;
+
+	if (len % 2)
+		start += 1;
+
+	for (i = start; i < len; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar(10);
+}
