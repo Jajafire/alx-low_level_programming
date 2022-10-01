7b61eab1981aa5ba81b5467531ad4789c5249aae
Sammy <sammyawoyinka10@gmail.com>
2022-10-01 04:11:23 -0700

    i

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
-	{
+	for (i = 1; i <= 10	{
 		if (i % 3 == 0 && i % 5 != 0)
 		{
 			printf(" Fizz");
 		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf(" FizzBuzz");
 		} else if (i == 1)
@@ -29,5 +31,5 @@ int main(void)
 	}
 	printf("\n");
 
-	return (0);
+	return (0)
 }
