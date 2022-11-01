cf97da29e31cf1645efd53559d6af3ea6a39a315
Sammy <sammyawoyinka10@gmail.com>
2022-11-01 13:52:19 -0700

    l

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -52,9 +52,9 @@ void close_file(int fd)
  * Return: 0 on success.
  *
  * Description: If the argument count is incorrect - exit code 97.
- * 		If file_from does not exist or cannot be read - exit code 98.
- * 		If file_to cannot be created or written to - exit code 99.
- * 		If file_to or file_from cannot be closed - exit code 100.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
@@ -84,11 +84,11 @@ int main(int argc, char *argv[])
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (r > 0);
+	   } while (r > 0);
+	
+		free(buffer);
+		close_file(from);
+		close_file(to);
 
-	free(buffer);
-	close_file(from);
-	close_file(to);
-
-	return (0);
+		return (0);
 }
